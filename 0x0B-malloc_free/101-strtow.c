7e2b72dff347221f37bb3a88776501c89f38860c
DarkVoid01 <arnoldeny15@gmail.com>
2022-10-03 13:19:51 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * _strlen - find length of a string
@@ -6,13 +6,13 @@
  * Return: int
  */
 
+
 int _strlen(char *s)
 {
-	int size = 0;
-
-	for (; s[size] != '\0'; size++)
-		;
-	return (size);
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
 }
 
 /**
@@ -24,83 +24,89 @@ int _strlen(char *s)
 
 char *str_addChar (char *str, char c)
 {
-	int size, i;
+int size, i;
+char *m;
 
-	char *m;
+size = _strlen(str);
 
-	m = malloc((size +1) * sizeof(char) + 1);
+m = malloc((size + 1) * sizeof(char) + 1);
+if (m == 0)
+	return (0);
 
-	if (m == 0)
-		return (0);
-	for (i = 0; i <= size; i++)
-		m[i] = str[i];
-	m[i + 1] = c;
+for (i = 0; i <= size; i++)
+	m[i] = str[i];
 
-	m[i + 2] = '\0';
+m[i + 1] = c;
+m[i + 2] = '\0';
 
-	return (m);
+return (m);
 }
+
 
 /**
  * *nbr_spaces - return the number of occurent of a string
- * @str: the string passed as argument.
- * Return: tokens
+ * @s: string to check
+ * Return: int
  */
 
-char **strtow(char *str)
+unsigned int nbr_spaces(char *s)
 {
-	int i;
-	int spaces = nbr_spaces(str);
-	char **tokens = NULL;
-	char *token;
-	int checkingSpace = 0;
-	int word = 0;
+	int i, cmpt = 0;
 
-	if (!tokens)
+	for (i = 0; s[i + 1] != '\0'; i++)
 	{
-		printf("Failed");
-		return (0);
+		if (s[i]  == ' ' && s[i + 1] != ' ')
+			cmpt++;
 	}
 
-	printf("looping");
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
-		{
-			if (checkingSpace == 0)
-			{
-				word++;
-				checkingSpace = 1;
-			}
-		}
-		else
-		{
-			printf("1");
-			token = tokens[word];
-			free(tokens[word]);
-			str_addChar(token, str[i]);
-			checkingSpace = 0;
-		}
-	}
-
-	tokens[i] = NULL;
-
-	return (tokens);
+	return (cmpt + 1);
 }
 
 
+/**
+  *strtow - split a sentence into multiple words.
+  *@str: the string passed as argument.
+  *Return: tokens
+  */
+char **strtow(char *str)
+{
+int i;
+int spaces = nbr_spaces(str);
+char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
+char *token;
+int checkingSpace = 0;
+int word = 0;
 
+if (!tokens)
+{
+	printf("Failed");
+	return (0);
+}
+	
 
+printf("looping");
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (str[i] == ' ')
+	{
+		if (checkingSpace == 0)
+		{
+			word++;
+			checkingSpace = 1;
+		} 
+	}
+	else
+	{
+		printf("1");
+		token = tokens[word];
+		free(tokens[word]);
+		str_addChar(token, str[i]);
+		checkingSpace = 0;
+	}
 
+}
 
+tokens[i] = NULL;
 
-
-
-
-
-
-
-
-
-
-
+return (tokens);
+}
