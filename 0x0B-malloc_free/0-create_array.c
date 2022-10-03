842ae56d84205f4ef03f4cd64c3c3c08bc3ff7b3
DarkVoid01 <arnoldeny15@gmail.com>
2022-10-03 08:33:21 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *m = malloc(size);
-       if (size == 0 || m == 0)
-       return (0);
+	if (size == 0 || m == 0)
+		return (0);
+	while (size--)
+		m[size] = c;
 
-while (size--)
- m[size] = c;
-
-return (m);
+	return (m);
 }
-
